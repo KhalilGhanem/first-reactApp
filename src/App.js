d05da6eb4bafa89cb5bb5f1e7cd300e15f01a4a8
Khalil Ghanem <khalil_ghanem7@yahoo.com>
2021-06-09 16:59:13 +0300

    lab04 done

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer ';
 import Main from './components/main';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
 
 class App extends React.Component {
   render() {
